b5928b160d560cfdec596e278a73600af50444a9
Nejc <nejcsk@hotmail.com>
2025-08-12 12:50:41 +0000

    Formatting

--- src/lib/components/WikiSectionDisplay.svelte
+++ src/lib/components/WikiSectionDisplay.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import type WikiSection from '$lib/wikipedia/WikiSection';
-	import WikiSectionDisplay from './WikiSectionDisplay.svelte';
-	export let section: WikiSection;
+    import type WikiSection from '$lib/wikipedia/WikiSection';
+    import WikiSectionDisplay from './WikiSectionDisplay.svelte';
+    export let section: WikiSection;
 </script>
 
 <section>
-	<h1>{section.heading}</h1>
-	{#each section.paragraphs as paragraph}
-		<p>{paragraph}</p>
-	{/each}
-	{#if section.subsections.length > 0}
-		<div class="subsections">
-			{#each section.subsections as subsection}
-				<WikiSectionDisplay section={subsection} />
-			{/each}
-		</div>
-	{/if}
+    <h1>{section.heading}</h1>
+    {#each section.paragraphs as paragraph}
+        <p>{paragraph}</p>
+    {/each}
+    {#if section.subsections.length > 0}
+        <div class="subsections">
+            {#each section.subsections as subsection}
+                <WikiSectionDisplay section={subsection} />
+            {/each}
+        </div>
+    {/if}
 </section>
